b27942a891b9498f8ca0622c37dff77a95d78f30
Sarthak Rathor <161385058+sarthakrathor@users.noreply.github.com>
2025-01-19 21:06:10 +0530

    Time: 0 ms (100%), Space: 42.2 MB (15.98%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,12 +3,9 @@ class Solution {
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
-                int temp = nums[i];
-                nums[j] = nums[i];
-                nums[i] = temp;
+                nums[j]=nums[i];
                 j++;
-            }
-            
+            }  
         }
         return j;
     }
