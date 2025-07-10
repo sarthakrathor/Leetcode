4e37dc406bd70feed514fc8ff2688da1bb5bd8f2
Sarthak Rathor <161385058+sarthakrathor@users.noreply.github.com>
2025-07-10 09:27:34 +0530

    Time: 35 ms (15.04%), Space: 46.9 MB (5.2%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        Integer[][] dp = new Integer[coins.length+1][amount+1];
+        for (int j = 0; j <= amount; j++) {
+            dp[0][j] = Integer.MAX_VALUE-1; 
+        }
+        dp[0][0] = 0;
+
+        for (int i = 1; i <= coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (coins[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+                }
+            }
+        }
+
+        return dp[coins.length][amount] > amount? -1 : dp[coins.length][amount];
+
+    }
+}
